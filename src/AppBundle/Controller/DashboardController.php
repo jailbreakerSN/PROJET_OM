8ac855a4dec91d395d40054755ff9fae2fe65762
Dame NDIAYE <dameleprince@gmail.com>
2018-02-24 10:54:21 +0000

    Initialisation du projet OM

---
+++ src/AppBundle/Controller/DashboardController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+
+class DashboardController extends Controller
+{
+    /**
+     * @Route("/", name="homepage")
+     * @return RedirectResponse
+     */
+    public function indexAction()
+    {
+        return new RedirectResponse($this->generateUrl('admin_dashboard'));
+    }
+
+    /**
+     * @Route("/dashboard", name="admin_dashboard")
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function dashboardAction()
+    {
+        return $this->render('default/dashboard.html.twig');
+    }
+
+    /**
+     * @Route("/page", name="page")
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function pageAction()
+    {
+        return $this->render('default/page.html.twig');
+    }
+}
